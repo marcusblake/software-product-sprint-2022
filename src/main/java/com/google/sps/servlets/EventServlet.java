d370be2c59522acb9cac3f0f0ce93bab34646a62
Katherine Yu <katherineyangyu@gmail.com>
2022-07-15 18:12:06 +0000

    Remove unnecessary import from EventServlet

--- src/main/java/com/google/sps/servlets/EventServlet.java
+++ src/main/java/com/google/sps/servlets/EventServlet.java
@@ -21,7 +21,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 import java.util.Map;
-import java.lang.reflect.Array;
 import java.lang.reflect.Type;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.Gson;
